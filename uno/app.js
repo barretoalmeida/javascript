4b9e52a11303ebef8bf54f26e1348bf946f21304
barretoalmeida <barretoalmeidagabriely@gmail.com>
2025-12-02 21:04:42 -0300

    finalização do projeto

--- uno/app.js
+++ uno/app.js
@@ -5,5 +5,29 @@
 
 function sortear() {
     let = jogar = Math.floor(Math.random() * 6)
+    console.log(jogar)
 
+    switch(jogar) {
+        case 0:
+            document.getElementById('uno').src = "./img/azulquatro.png"
+            break
+        case 1: 
+            document.getElementById('uno').src = "./img/uno2.png"
+            break
+        case 2:
+            document.getElementById('uno').src = "./img/veruno.png"
+            break
+        case 3:
+            document.getElementById('uno').src = "./img/unorever.png"
+            break
+        case 4:
+            document.getElementById('uno').src = "./img/umuno.png"
+            break
+        case 5:
+            document.getElementById('uno').src = "./img/quatro.png"
+            break
+    }
+}
+function voltar() {
+        document.getElementById('uno').src = "./img/frente.jpg"
 }
